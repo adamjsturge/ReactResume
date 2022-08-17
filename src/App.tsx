1445d902afc387b8e3ad953d0e4ba69dfbef7351
adamjsturge <adamjsturge@gmail.com>
2022-08-17 00:24:10 -0400

    Finish Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
 import * as React from 'react';
 import Home from './pages/Home';
+import parseRoute from './lib/parse-route';
 
 export default class App extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
     this.state = {
-      route: 'home'
+      route: parseRoute(window.location.hash),
     };
     // this.functionName = this.functionName.bind(this);
   }
 
 
   componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      let route = parseRoute(window.location.hash);
+      this.setState({ route });
+    });
   }
 
   renderPage() {
-    const route = this.state.route;
-    switch (route) {
+    const { path, params } = this.state.route;
+    switch (path.toLowerCase()) {
       case 'home':
         return (
           <>
---
+++ src/lib/parse-route.ts
@@ -0,0 +1,14 @@
+export default function parseRoute(hashRoute:string) {
+    if (hashRoute.startsWith('#')) {
+      hashRoute = hashRoute.replace('#', '');
+      const [path, queryString] = hashRoute.split('?');
+      const params = new URLSearchParams(queryString);
+      return { path, params };
+    } else {
+        return {
+            path: 'home',
+            params: {}
+        }
+    }
+  }
+  
